8bd2e44224b2d39aac0a36c422fe380b4ad367fe
andrew herculano <andrewherculano@hotmail.com>
2022-01-12 20:18:29 -0300

    obtendo e inserindo informações da data na tela

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,46 @@
 const clockHours = document.querySelector('.clock-hours')
+const infoDate = document.querySelector('.clock-info')
 
-const getHour = () => {
-  const present = new Date()
-  const hours = present.getHours()
-  const minutes = present.getMinutes()
-  const seconds = present.getSeconds()
+const daysOfWeek = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabádo']
+const months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 
+const getPresentDate = () => new Date()
+
+const formatNumber = number =>
+  `${String(number).length === 1 ? `0${number}` : `${number}`}`
+
+const clockHoursHTML = (hours, minutes, seconds) => {
   clockHours.innerHTML = `
     <span>${hours}</span> :
-    <span>${hours}</span> :
+    <span>${minutes}</span> :
     <span>${seconds}</span>
   `
 }
 
-setInterval(getHour, 1000);
+const updateClock = () => {
+  const present = getPresentDate()
+  const hours = formatNumber(present.getHours())
+  const minutes = formatNumber(present.getMinutes())
+  const seconds = formatNumber(present.getSeconds())
+
+  clockHoursHTML(hours, minutes, seconds)
+}
+
+const infoDateHTML = (dayOfWeek, dayOfMonth, month, year) => {
+  infoDate.innerHTML = `
+  <p>${dayOfWeek}, ${dayOfMonth} de ${month} de ${year}</p>
+  `
+}
+
+const updateInfoDate = () => {
+  const present = getPresentDate()
+  const dayOfWeek = daysOfWeek[present.getDay()]
+  const dayOfMonth = formatNumber(present.getDate())
+  const month = months[present.getMonth()]
+  const year = present.getFullYear()
+
+  infoDateHTML(dayOfWeek, dayOfMonth, month, year)
+}
+
+setInterval(updateClock, 1000);
+updateInfoDate()
